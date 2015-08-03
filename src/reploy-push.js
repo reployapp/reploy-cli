601e11a214a9e09347666d7221d3d01df0eb12fd
Joshua Sierles <joshua@diluvia.net>
2015-08-03 19:36:20 +0200

    add option to skip js bundle step. report errors frmo json when they happen.

--- src/reploy-push.js
+++ src/reploy-push.js
@@ -8,19 +8,30 @@ import fs  from 'fs';
 import request from 'superagent';
 import {appConf, appVersion} from './environment';
 
-// console.log("Starting bundle")
-// var bundle = spawnSync('node', [path.join(process.cwd(), 'node_modules/react-native/local-cli/cli.js'), 'bundle']);
-// console.log(bundle.stdout.toString());
+program
+  .option('-s, --skip_bundle', 'Skip javascript bundle step')
+  .parse(process.argv);
 
+
+if (program.skip_bundle) {
+  console.log("Skipping javascript bundle step.")
+} else {
+  console.log("Starting bundle...");
+  var bundle = spawnSync('node', [path.join(process.cwd(), 'node_modules/react-native/local-cli/cli.js'), 'bundle']);
+  console.log(bundle.stdout.toString());
+}
+
+console.log("Uploading bundle...")
 var url = `http://reploy.io/apps/${appConf.app.id}/${appVersion}/js_versions`;
-console.log(url);
+
 request
   .post(url)
   .attach('jsbundle', path.join(process.cwd(), 'iOS/main.jsbundle'))
   .end(function(err, res) {
     if (res.ok) {
-     console.log('yay got ' + JSON.stringify(res.body));
+     console.log('Done!')
+     console.log(JSON.stringify(res.body));
     } else {
-     console.log('Oh no! error ' + res.text);
+     console.log(res.body);
     }
   });
