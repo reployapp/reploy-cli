0bd4966eae461a40c372d6b150e7e86aa84f457e
Joshua Sierles <joshua@diluvia.net>
2016-03-16 01:33:12 +0100

    rename create-app to create. Closes #12.

--- src/reploy.js
+++ src/reploy.js
@@ -6,10 +6,10 @@ let command = null;
 
 let reploy = program
   .version('0.1.0')
-  .command('setup', 'Configure credentials for the Reploy API')
+  .command('setup', 'Configure your Reploy credentials')
   .command('list-apps', 'List javascript bundle versions')
-  .command('create-app', 'Create this app on Reploy')
-  .command('push-binary', 'Push an application binary')
+  .command('create', 'Add a React Native app')
+  .command('push-build', 'Push an iOS or Android build')
   .command('push-js', 'Build and push a javascript bundle')
   .command('list-js', 'List javascript bundle versions');
 
