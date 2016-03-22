f3123cbbb77e42854ce879e19688c6f72aa4b66f
Christopher Dro <hello@blick-labs.com>
2016-03-22 10:09:26 -0700

    [Setup] Validation when asking for email

--- src/reploy-setup.js
+++ src/reploy-setup.js
@@ -11,7 +11,7 @@ import api from './api';
 
 if (!globalConf.auth) {
 
-  var email = readlineSync.question('Enter your email address:')
+  var email = readlineSync.questionEMail('Enter your email address:', {limitMessage: 'Please enter a valid email.'})
   // TODO: add an 'email verification code' to login here
 } else {
   cli.ok(`You're already setup for reploy! To start over, remove the ~/${configFilename} file.`)
