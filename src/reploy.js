aa980f99289632939be02064a332f6909d1de555
Joshua Sierles <joshua@diluvia.net>
2016-03-28 20:13:32 +0200

    remove admin stuff

--- src/reploy.js
+++ src/reploy.js
@@ -2,9 +2,7 @@
 
 import program from 'commander';
 
-let command = null;
-
-let reploy = program
+let command = program
   .version('0.1.0')
   .command('setup', 'Configure your Reploy credentials')
   .command('list-apps', 'List javascript bundle versions')
@@ -12,16 +10,5 @@ let reploy = program
   .command('push-build', 'Push an iOS or Android build')
   .command('push-js', 'Build and push a javascript bundle')
   .command('list-js', 'List javascript bundle versions');
-
-if (process.env.REPLOY_ADMIN) {
-  command = reploy
-  .command('query', 'Run a graphql query from a file')
-  .command('delete-all', 'Delete all data!')
-  .command('list-hooks', 'List Reindex webhooks')
-  .command('create-devices', 'Create devices')
-  .command('invite-beta', 'Invite beta user');
-} else {
-  command = reploy;
-}
 
 command.parse(process.argv);
