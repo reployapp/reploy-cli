d40c589c1a150b7e5ce4c85cbf020869e80847fc
Joshua Sierles <joshua@diluvia.net>
2016-03-28 20:17:50 +0200

    fetch version from package.json

--- src/reploy.js
+++ src/reploy.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node --harmony
 
 import program from 'commander';
+import {version} from '../package.json';
 
 let command = program
-  .version('0.1.0')
+  .version(version)
   .command('setup', 'Configure your Reploy credentials')
   .command('list-apps', 'List javascript bundle versions')
   .command('create', 'Add a React Native app')
