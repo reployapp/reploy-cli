7d1986a016fb1a73221d2c22be7b9ff9ba72928f
Christopher Dro <hello@blick-labs.com>
2016-03-29 00:55:46 -0700

    [push-build] Use xcodebuild for iOS with new utility methods.

--- src/reploy-push-build.js
+++ src/reploy-push-build.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import { spawnSync } from 'child_process';
+import homedir from 'os-homedir';
+
 import path from 'path';
 import fs  from 'fs';
-import {mutation, query, getApplication} from './api';
-import {appConf, appVersion} from './environment';
-import {find, filter} from 'lodash';
-import homedir from 'os-homedir';
-import FormData from 'form-data';
-import {capitalize} from './util';
-import cli from 'cli';
+
+import { appConf } from './environment';
+import { capitalize, getDefaultSimulator, getXcodeProject } from './util';
+import parseCommandLine from './util/parseCommandLine';
+
+import { find, filter } from 'lodash';
+import { getProjectName, platformPrompt } from './util';
+import { buildPathAndroid, buildPathIos, uploadBuild } from './util/postBuildUpload';
+
+import { spawnSync } from 'child_process';
 
 program
   .option('-p, --platform [platform]', 'Platform: "ios" or "android"')
@@ -21,12 +25,6 @@ const platform = program.platform || 'ios';
 
 const superagent = require('superagent-promise')(require('superagent'), Promise);
 
-const buildDirIos = path.join(homedir(), '/Library/Developer/Xcode/DerivedData');
-const buildPathIos = `/tmp/${projectName()}-ios.zip`;
-
-const buildPathAndroid = path.join(process.cwd(), '/android/app/build/outputs/apk/app-release.apk');
-
-
 if (!fs.existsSync(appConf.__filename)) {
   console.log(`\nCouldn't find the Reploy config file named .reploy at the application root.\nDid you run 'reploy create'?\n`);
   process.exit(1);
@@ -35,33 +33,38 @@ if (!fs.existsSync(appConf.__filename)) {
 async function run() {
 
   if (platform == 'ios') {
-    createBuildZipFile();
+    buildIOS();
   } else {
     buildAndroid();
   }
+  uploadBuild(platform);
+}
 
-  const buildPath = platform == 'ios' ? buildPathIos : buildPathAndroid;
+function buildIOS() {
+  let xcodeProject = getXcodeProject();
+  let selectedSimulator = getDefaultSimulator();
 
-  let application = await getApplication(appConf.app.id);
+  const xcodebuildArgs = [
+    xcodeProject.isWorkspace ? '-workspace' : '-project', `./ios/${xcodeProject.name}`,
+    '-scheme', getProjectName(),
+    '-destination', `id=${selectedSimulator.udid}`,
+    '-configuration', 'Release',
+    'CODE_SIGNING_REQUIRED=NO',
+    'CONFIGURATION_BUILD_DIR=/tmp/' + getProjectName(),
+  ];
 
-  try {
-
-    let uploadId = await uploadBuild(buildPath);
-    let appetizeData = null;
-
-    console.log(`Uploading ${platform} build to Reploy...`);
-    let appetizePrivateKey = application[`appetizePrivateKey${capitalize(platform)}`];
-    if (appetizePrivateKey) {
-      appetizeData = await uploadToAppetize(uploadId, {appetizePrivateKey, platform});
-    } else {
-      appetizeData = await uploadToAppetize(uploadId, {platform});
-      await addAppetizeIdToReploy(appetizeData, platform);
+  if (!program.skip) {
+    console.log('Building project...');
+    let buildCommand = spawnSync('xcodebuild', xcodebuildArgs, {stdio: 'inherit'});
+    if (buildCommand.status != 0) {
+      console.log(`Build failed: ${buildCommand.error}`);
+      process.exit(1);
     }
-
-    addBuildtoReploy(uploadId, appetizeData, platform);
-  } catch (error) {
-    console.log(error);
   }
+
+  console.log("Zipping build before upload...")
+  process.chdir(`/tmp/${getProjectName()}`);
+  let zip = spawnSync('zip', ['-r', buildPathIos,  '.']);
 }
 
 function buildAndroid() {
@@ -71,151 +74,6 @@ function buildAndroid() {
     spawnSync('./gradlew', ['assembleRelease'], {stdio: 'inherit'});
   } else {
     console.log("Skipping android build...");
-  }
-}
-
-async function addAppetizeIdToReploy(appetizeData, platform) {
-  let data = {
-    id: appConf.app.id,
-  };
-
-  console.log(appetizeData);
-  data[`appetizePublicKey${capitalize(platform)}`] = appetizeData.publicKey;
-  data[`appetizePrivateKey${capitalize(platform)}`] = appetizeData.privateKey;
-
-  await mutation('updateApplication', data);
-}
-
-async function addBuildtoReploy(uploadId, appetizeData, platform) {
-  let response = await mutation('createBinaryUpload', {
-    uploadId: uploadId,
-    user: appConf.app.user,
-    platform: platform,
-    application: appConf.app.id,
-    versionCode: appetizeData.versionCode,
-    createdAt: '@TIMESTAMP',
-  });
-}
-
-function findFileIn(name, path) {
-  let files = fs.readdirSync(path);
-
-  let file = find(files, (filename) => {
-    return filename.indexOf(name) > -1;
-  });
-
-  return file ? `${path}/${file}` : null;
-}
-
-function projectName() {
-  let file = iosProjectFile();
-  let segments = file.split('/');
-  return segments[segments.length - 1].split('.')[0];
-}
-
-function latestBuildPath() {
-
-  let simulatorBuildPaths = filter(fs.readdirSync(buildDirIos), (path) => {
-    return path.indexOf(projectName()) > -1;
-  });
-
-  if (simulatorBuildPaths.length == 0) {
-    return null;
-  }
-
-  simulatorBuildPaths.sort(function(a, b) {
-    return fs.statSync(`${buildDirIos}/${b}`).mtime.getTime() - fs.statSync(`${buildDirIos}/${a}`).mtime.getTime();
-  });
-
-  return simulatorBuildPaths[0];
-}
-
-function iosProjectFile() {
-  let file = null;
-
-  file = findFileIn('xcodeproj', path.join(process.cwd(), 'ios'));
-
-  if (!file) {
-    file = findFileIn('xcodeproj', process.cwd());
-  }
-  return file;
-}
-
-function createBuildZipFile() {
-
-  spawnSync('mkdir', ['-p', `/tmp/${projectName()}.xcode`]);
-
-  let buildArguments = `CODE_SIGNING_REQUIRED=NO
-CODE_SIGN_IDENTITY=
-PROVISIONING_PROFILE=
-CONFIGURATION_BUILD_DIR=/tmp/${projectName()}.xcode
--destination
-platform=iOS Simulator,name=iPhone 6,OS=9.2
--sdk
-iphonesimulator
--configuration
-Release
--project
-${iosProjectFile()}
--scheme
-${projectName()}
-build`;
-
-
-  if (!program.skip) {
-    console.log('Building project...');
-    let buildArray = buildArguments.split('\n');
-    let buildCommand = spawnSync('xctool', buildArray, {stdio: 'inherit'});
-    if (buildCommand.status != 0) {
-      console.log(`Build failed: ${buildCommand.error}`);
-      process.exit(1);
-    }
-  }
-
-  console.log("Zipping build before upload...")
-  process.chdir(`/tmp/${projectName()}.xcode`);
-  let zip = spawnSync('zip', ['-r', buildPathIos,  '.']);
-}
-
-async function uploadBuild(filePath) {
-
-  console.log(`Uploading build from ${filePath}...`)
-  let size = fs.statSync(filePath).size;
-
-  try {
-    let response = await superagent.post('https://upload.uploadcare.com/base/')
-      .field('UPLOADCARE_PUB_KEY', '9e1ace5cb5be7f20d38a')
-      .attach('file', filePath)
-      .on('progress', (progress) => {
-        cli.progress(progress.loaded / progress.total);
-      });
-
-    return (response.body.file);
-
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function uploadToAppetize(uploadId, options = {appetizePrivateKey: null, platform: 'ios'}) {
-
-  console.log(`Finalizing upload for platform ${options.platform}`);
-
-  let params = {
-    url: `https://ucarecdn.com/${uploadId}/file.zip`,
-    platform: options.platform,
-  };
-
-  if (options.appetizePrivateKey) {
-    params.privateKey = options.appetizePrivateKey;
-  }
-
-  try {
-    let result = await superagent.post('https://webtask.it.auth0.com/api/run/wt-joshua-diluvia_net-0/createOnAppetize')
-    .send({params});
-    return result.body;
-  } catch (error) {
-    console.log(error);
   }
 }
 
