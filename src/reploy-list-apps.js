4313a8b4d44fb09d06148df326a0e673ebabc737
Christopher Dro <hello@blick-labs.com>
2016-05-05 18:44:27 -0700

    [list-apps] Handle case where user has no apps. Add table view for current apps with more data. Closes #21

--- src/reploy-list-apps.js
+++ src/reploy-list-apps.js
@@ -1,22 +1,58 @@
 #!/usr/bin/env node
 
 import { query } from './api';
+import Table from 'cli-table';
+import colors from 'colors';
+
+const table = new Table({
+  head: [
+    colors.cyan('Name'),
+    colors.cyan('Invitees'),
+    colors.cyan('Screenshots'),
+    colors.cyan('Version')
+  ],
+  colWidths: [25, 15, 15, 15]
+});
+
 async function run() {
   try {
     let result = await query(`
-        user {
-          applications {
-            edges {
-              node {
-                name
+      user {
+        applications {
+          count,
+          nodes {
+            id
+            name
+            screenshots {
+              count,
+            }
+            invitedUsers {
+              count
+            }
+            binaryUploads(last: 1) {
+              nodes {
+               versionCode
               }
             }
           }
-        }`);
+        }
+      }`);
 
-    result.user.applications.edges.forEach((app) => {
-      console.log(app.node.name)
-    });
+    const { applications } = result.user;
+
+    if (applications.count === 0) {
+      console.log('Uh oh! No applications found. \nPlease make sure you\'ve created an app and that your accounts token is set in /~.reploy');
+    } else {
+      applications.nodes.forEach((app) => {
+        table.push([
+          app.name,
+          app.invitedUsers.count,
+          app.screenshots.count,
+          app.binaryUploads.nodes[0].versionCode
+        ]);
+      });
+      console.log(table.toString());
+    }
   } catch(error) {
     console.log(error);
   }
